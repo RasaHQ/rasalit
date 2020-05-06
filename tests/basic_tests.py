ccba24e749e86fb8cc9a337e39bada076b2c8cf2
Vincent <vincentwarmerdam@gmail.com>
2020-05-06 14:43:47 +0200

    1 more\
    g

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -2,6 +2,6 @@ import pytest
 from rasalit.__main__ import app_path
 
 
-@pytest.mark.parametrize("f", ["viewresults.py", "html/diet"])
+@pytest.mark.parametrize("f", ["viewresults.py", "html/diet", "html/parallelcoords"])
 def test_can_find_files(f):
     app_path(f)
