891fcf4593a5dc03afbf3a92f40911c272f1a69c
Vincent <vincentwarmerdam@gmail.com>
2020-09-03 15:19:17 +0200

    tests-green

--- tests/test_common.py
+++ tests/test_common.py
@@ -2,6 +2,6 @@ import pytest
 from rasalit.common import app_path
 
 
-@pytest.mark.parametrize("f", ["app.py", "html/diet"])
+@pytest.mark.parametrize("f", ["html/diet"])
 def test_can_find_files(f):
     app_path(f)
