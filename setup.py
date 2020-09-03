2dd15686cca65388d24f1be16ad6e9a949481f05
Vincent <vincentwarmerdam@gmail.com>
2020-09-03 16:08:30 +0200

    typer

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 from rasalit import __version__
 
-base_packages = ["streamlit>=0.57.3", "pyyaml>=5.3.1", "pandas>=1.0.3", "altair>=4.1.0"]
+base_packages = [
+    "streamlit>=0.57.3",
+    "pyyaml>=5.3.1",
+    "pandas>=1.0.3",
+    "altair>=4.1.0",
+    "typer>=0.3.0",
+]
 
 dev_packages = ["flake8>=3.6.0", "pytest>=4.0.2", "pre-commit>=2.7.1", "black>="]
 
