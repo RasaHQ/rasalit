96d6d9bbbc5c0855d6d27875eea45e71b190187c
Drew Silcock <drew.silcock@stfc.ac.uk>
2021-10-21 16:54:47 +0100

    Fix linting error.

--- setup.py
+++ setup.py
@@ -11,9 +11,8 @@ base_packages = [
     "spacy>=2.3.2",
     "tensorflow>=2.3.1",
     "nlpaug>=1.1.2",
-
     # Backfill for Python < 3.7.
-    "importlib-resources>=5.3.0"
+    "importlib-resources>=5.3.0",
 ]
 
 dev_packages = ["flake8>=3.6.0", "pytest>=4.0.2", "pre-commit>=2.7.1", "black"]
